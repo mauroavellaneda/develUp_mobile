e60e2e675f841c6521e6899f49f67368cd3392af
Mauro Avellaneda <mauroeavellaneda@gmail.com>
2020-11-10 08:33:53 +0100

    Mocking 'visitorCanSeePublicIndex'

--- e2e/visitorCanSeePublicIndex.e2e.js
+++ e2e/visitorCanSeePublicIndex.e2e.js
@@ -4,19 +4,18 @@ describe("Visitor can see public index", () => {
   beforeAll(async () => {
     await reloadApp();
   });
-  xit("is expected to have elements within assignment card", async () => {
-    await expect(element(by.id("assignment-100"))).toExist();
-    await expect(element(by.id("title-100"))).toHaveLabel("Build a Web page");
-    await expect(element(by.id("budget-100"))).toHaveLabel("$ 500");
-    await expect(element(by.id("skills-100"))).toHaveLabel(
-      "Skills: Ruby JavaScript"
-    );
-    await expect(element(by.id("points-100"))).toHaveLabel("Points: 320");
+  it("is expected to have elements within assignment card", async () => {
+    await expect(element(by.id("assignment-45"))).toExist();
+    await expect(element(by.id("title-45"))).toHaveLabel("Build a Web page");
+    await expect(element(by.id("budget-45"))).toHaveLabel("$ 500");
+    await expect(element(by.id("points-45"))).toHaveLabel("Points: 320");
   });
-  xit("is expected to have ability to scroll up", async () => {
+  it("is expected to have ability to scroll up", async () => {
     await expect(element(by.id("scroll"))).toExist();
     await element(by.id("scroll")).swipe("up");
-    await expect(element(by.id("title-14"))).toHaveLabel("Build a Full Stack Rails application");
+    await expect(element(by.id("title-14"))).toHaveLabel(
+      "Build a Full Stack Rails application"
+    );
     await expect(element(by.id("budget-14"))).toHaveLabel("$ 300");
     await expect(element(by.id("skills-14"))).toHaveLabel("Skills: Ruby");
     await expect(element(by.id("points-14"))).toHaveLabel("Points: 220");
